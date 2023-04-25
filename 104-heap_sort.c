7ef57bcef920d75e07507f2dfdc3836052f69271
kaytee07 <kofi.taylor07@gmail.com>
2023-04-25 18:39:12 +0000

    all

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -9,18 +9,19 @@
  */
 void sift_down(int *array, size_t start, size_t end, size_t size)
 {
-size_t root = start;
+int temp;
+ size_t root = start, child, swap;
 while (2 * root + 1 <= end)
 {
-size_t child = 2 * root + 1;
-size_t swap = root;
+child = 2 * root + 1;
+swap = root;
 if (array[swap] < array[child])
 swap = child;
 if (child + 1 <= end && array[swap] < array[child + 1])
 swap = child + 1;
 if (swap == root)
 return;
-int temp = array[root];
+temp = array[root];
 array[root] = array[swap];
 array[swap] = temp;
 print_array(array, size);
@@ -35,15 +36,16 @@ root = swap;
  */
 void heap_sort(int *array, size_t size)
 {
+int start, end, temp; 
 if (size <= 1)
 return;
 
-for (int start = (size - 2) / 2; start >= 0; start--)
+for (start = (size - 2) / 2; start >= 0; start--)
 sift_down(array, start, size - 1, size);
 
-for (int end = size - 1; end > 0; end--)
+for (end = size - 1; end > 0; end--)
 {
-int temp = array[end];
+temp = array[end];
 array[end] = array[0];
 array[0] = temp;
 print_array(array, size);
